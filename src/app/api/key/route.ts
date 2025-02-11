9683516bf296128c93258ba52e69ae47dce84f28
Aditya Bisht <adityabisht64@gmail.com>
2025-02-11 13:52:12 +0530

    feat(api): increase rate limit to 100 requests per second

--- src/app/api/key/route.ts
+++ src/app/api/key/route.ts
@@ -12,7 +12,7 @@ export type DomainSearchResults = {
   value: string;
 };
 
-const rateLimiter = new RateLimiterMemory({ points: 5, duration: 10 });
+const rateLimiter = new RateLimiterMemory({ points: 100, duration: 1 });
 
 export async function GET(request: NextRequest) {
   try {
