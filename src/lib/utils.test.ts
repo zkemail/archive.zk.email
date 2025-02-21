426858cbeff324f445b1a0fc18c0f62fa31865a7
sukhman <sukhmansinghsaluja@gmail.com>
2025-02-21 16:45:21 +0530

    Add ci test to github actions

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,18 +1,23 @@
-import { expect, test } from 'vitest'
-import { parseDkimTagList } from './utils';
+import { expect, test } from "vitest";
+import { parseDkimTagList } from "./utils";
 
-test('parseDkimTagList', () => {
-	expect(parseDkimTagList(' k=rsa;b=c; =foo   ; hello; b=duplicate_value_for_b; p=abcd12345;;;k2=v2')).toStrictEqual({
-		k: 'rsa',
-		p: 'abcd12345',
-		b: 'c',
-		k2: 'v2',
-	});
+test("parseDkimTagList", () => {
+  expect(
+    parseDkimTagList(
+      " k=rsa;b=c; =foo   ; hello; b=longest_value_for_b; p=DKIM1; p=abcd12345;;;k2=v2"
+    )
+  ).toStrictEqual({
+    k: "rsa",
+    p: "abcd12345",
+    b: "longest_value_for_b",
+    k2: "v2",
+  });
 
-	expect(parseDkimTagList('')).toStrictEqual({});
+  expect(parseDkimTagList("")).toStrictEqual({});
 
-	const tagList = parseDkimTagList('a=b; c=d');
-	expect(tagList.a).toBe('b');
-	expect(tagList.hasOwnProperty('c')).toBe(true);
-	expect(tagList.hasOwnProperty('f')).toBe(false);
-})
+  const tagList = parseDkimTagList("v=1; v=DKIM1; a=b; c=d");
+  expect(tagList.a).toBe("b");
+  expect(tagList.v).toBe("DKIM1");
+  expect(tagList.hasOwnProperty("c")).toBe(true);
+  expect(tagList.hasOwnProperty("f")).toBe(false);
+});
