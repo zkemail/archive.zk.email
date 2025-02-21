c3018ce7117c1b8283b94263dc6bd211c3cd37c0
sukhman <sukhmansinghsaluja@gmail.com>
2025-02-21 17:22:52 +0530

    Add real values in test

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,15 +1,15 @@
 import { expect, test } from "vitest";
 import { parseDkimTagList } from "./utils";
-
 test("parseDkimTagList", () => {
   expect(
     parseDkimTagList(
-      " k=rsa;b=c; =foo   ; hello; b=longest_value_for_b; p=DKIM1; p=abcd12345;;;k2=v2"
+      "v=1; k=rsa;b=c; =foo   ; hello; b=duplicate_value_for_b; p=DKIM1; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArkIVR9VjV+E605cxGymoapBUHX1ooeEFTJv9xCKgXQIgvAORT8BgzwGSYHMT/PFrVaHa3+6ESNKaSnPTIlVOWTddkOBBxlGz1+r/TEnCgD9JMD/0oHzQDj5v9+f+yMfcZo9Co1gH8Z23zvbI1ArO+YsBTAFuh4arIRVSzw+zTvrWZ/E9/sp71jaH4lrR+aLFhI2j3QN/jzugfBl/T+pHQlePJmlBGs9a7pkyMfQB1oYULdoxrxKoYob1GL7kdsMH5kqvvK8UyW2z1/3AhSoZAsFvjzuZUL182rhxZAAmP6n8u/o7Xugp7Ije+aEXXKBiZkqS9qNbDRjEkn2FaL/xkQIDAQAB;;;k2=v2"
     )
   ).toStrictEqual({
+    v: "1",
     k: "rsa",
-    p: "abcd12345",
-    b: "longest_value_for_b",
+    p: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArkIVR9VjV+E605cxGymoapBUHX1ooeEFTJv9xCKgXQIgvAORT8BgzwGSYHMT/PFrVaHa3+6ESNKaSnPTIlVOWTddkOBBxlGz1+r/TEnCgD9JMD/0oHzQDj5v9+f+yMfcZo9Co1gH8Z23zvbI1ArO+YsBTAFuh4arIRVSzw+zTvrWZ/E9/sp71jaH4lrR+aLFhI2j3QN/jzugfBl/T+pHQlePJmlBGs9a7pkyMfQB1oYULdoxrxKoYob1GL7kdsMH5kqvvK8UyW2z1/3AhSoZAsFvjzuZUL182rhxZAAmP6n8u/o7Xugp7Ije+aEXXKBiZkqS9qNbDRjEkn2FaL/xkQIDAQAB",
+    b: "duplicate_value_for_b",
     k2: "v2",
   });
 
